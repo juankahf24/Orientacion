2a3554b6205d39a4e242fb316ad981f5c82354da
juankahf24 <juanka.hf.24@gmail.com>
2026-05-22 11:05:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* MILITOPO · cache offline seguro v54 */
-const MILITOPO_CACHE = "militopo-root-offline-v54";
+const MILITOPO_CACHE = "militopo-root-offline-v55";
 const CORE_ASSETS = [
   "./",
   "./index.html",
